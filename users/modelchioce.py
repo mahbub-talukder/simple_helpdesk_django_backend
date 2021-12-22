e61ef72a840d1b0988c253d28eac4aa22f4c9371
mahbub_cgbd <mahbub.alum@cg-bd.com>
2021-12-23 01:14:47 +0600

    [init] helpdesk backend

---
+++ users/modelchioce.py
@@ -0,0 +1,21 @@
+PRIORITY_CHOICE = (
+    ('low','LOW'),
+    ('median','Median'),
+    ('high','High'),
+)
+
+RESOLVE_CHOICE = (
+    ('pending','Pending'),
+    ('in_progress','In-progress'),
+    ('review','Review'),
+    ('cancelled','Cancelled'),
+    ('done','Done'),
+)
+
+USER_TYPE_CHOICE = (
+    ('admin','Admin'),
+    ('employee','Employee'),
+    ('student','Student'),
+    ('admission_officer','Admission Officer'),
+    ('register_offier','Register Offier'),
+)
